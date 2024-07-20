5387e88da2daa78bf729d878c5ac3a50a7675912
Paulo Henrique <paulohenriqueribeiroalves@gmail.com>
2024-07-20 09:12:22 -0300

    Fiz a lista dos produtos que estão a venda, com os componentes separados e fiz o componente para guardar os itens selecionandos para o carrinho

---
+++ src/components/products/item.tsx
@@ -0,0 +1,44 @@
+'use client'
+import { Product } from "@/types/product";
+import { Button } from "../ui/button";
+import { useToast } from "../ui/use-toast";
+import { ToastAction } from "../ui/toast";
+import { useCardStore } from "@/stores/card-store";
+
+type Props = {
+    item: Product;
+}
+export const ProductItem = ({ item }: Props) => {
+    const { toast } = useToast();
+    const { upsertCartItem } = useCardStore(state => state);
+
+    const handleAddButton = () => {
+        //1-etapa: Adicionar o item no store
+        upsertCartItem(item, 1);
+        //2-etapa: Aviso que foi adicionando
+        toast({
+            title: 'Adicionado ao carrinho',
+            description: item.name,
+            action: <ToastAction altText="fechar">Fechar</ToastAction>
+        });
+    }
+    return(
+        <div>
+            <div className="rounded-md overflow-hidden">
+                <img
+                    src={item.image}
+                    alt={item.name}
+                    className="w-full h-32 object-cover"
+                />
+            </div>
+            <div className="mt-3 flex flex-col gap-2">
+                <p className="text-lg">{item.name}</p>
+                <p className="text-sm opacity-50">R$ {item.price.toFixed(2)}</p>
+                <Button
+                    variant="outline"
+                    onClick={handleAddButton}
+                >Adicionar</Button>
+            </div>
+        </div>
+    );
+}
---
+++ src/stores/card-store.ts
@@ -0,0 +1,35 @@
+import { Cart } from "@/types/cart";
+import { Product } from "@/types/product";
+import { create } from "zustand";
+
+type States = {
+    cart: Cart[];
+}
+type Actions = {
+    upsertCartItem: (product: Product, quantity: number) => void;
+}
+
+const initialStates: States = {
+    cart: []
+}
+
+export const useCardStore = create<States & Actions>()(set => ({
+    ...initialStates,
+    upsertCartItem: (product, quantity) => set(state => {
+        let newCart = state.cart;
+        //adicionando produtos que não tem no carrinho
+        let productIndex = newCart.findIndex(item => item.product.id === product.id);
+        if(productIndex < 0) {
+            newCart.push({ product, quantity: 0 });
+            productIndex = newCart.findIndex(item => item.product.id === product.id);
+        }
+        //função para adicionar itens no carrinho
+        newCart[productIndex].quantity += quantity;
+        //função para remover o produto do carrinho
+        if (newCart[productIndex].quantity <= 0) {
+            newCart = newCart.filter(item => item.product.id !== product.id);
+        }
+
+        return { ...state, cart: newCart }
+    })
+}));
